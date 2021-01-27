10f05a855c1dd6255950927c15948d87a802864f
CarterVader <59375706+CarterVader@users.noreply.github.com>
2021-01-27 10:39:27 -0500

    Add files via upload

---
+++ Assets/Game/Assets/Scripts/QuitHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitHandler : MonoBehaviour
+{
+    static QuitHandler instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Add to DontDestroyOnLoad
+        DontDestroyOnLoad(this);
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Cancel"))
+        {
+            Application.Quit();
+        }
+    }
+}
